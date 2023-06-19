79e76054a18165cde9f6d807ea6953046f2110e6
rafael roth <rafael2rooth@gmail.com>
2023-06-19 11:01:19 -0300

    aircrfaft um pouco mais feito

--- AirCraft.java
+++ AirCraft.java
@@ -1,24 +1,34 @@
 import java.util.Arrays;
-public class AirCraft
+public class AirCraft //tamanho 30 | 6
 {
     public static void main (String [] args)
     {
+        Seats seats = new Seats();
         boolean [][] aircraft = new boolean [30][6];
-        print(aircraft);
         
-        aircraft[1][3] = false;
+        
+        //aircraft[3][3] = true;
+        //printAircraft(aircraft);
+        
+        //LOOP PRINCIPAL
+        while(true) {
+            System.out.printf("bem-vindo ao comprador de passagens\nGostaria de comprar uma passaegem?\n[1] Voo3\n[2] Voo2\n[3] Voo1");
+            break;
+            
+        }
         
     }
-    public static void print(boolean aircraft [][])
+    public static void printAircraft(boolean aircraft [][])
     {
-        System.out.println(" A  B  C     D  E  F    ");
+        System.out.println("   A  B  C     D  E  F    ");
         for ( int i = 0; i < aircraft.length; i++)
         {
+            System.out.print(i + " ");
             for ( int j = 0; j < aircraft[i].length; j++)
             {
-                if (aircraft[i][j])
+                if (aircraft[i][j] == true) //se true! 
                 {
-                    System.out.print("[O]");
+                    System.out.print("[$]");
                 }// se estiver ocupado:
                 else
                 {
@@ -34,6 +44,6 @@ public class AirCraft
                 System.out.println();
             }
         }
-        System.out.println(" A  B  C     D  E  F    ");
+        System.out.println("    A  B  C     D  E  F    ");
     }
 }       

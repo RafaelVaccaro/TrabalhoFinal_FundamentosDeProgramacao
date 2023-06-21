db0b8f9ffc9ca41d1dfe6333278d19d5480e643c
Gustavo Teixeira <gustavohteixeira2904@gmail.com>
2023-06-21 10:48:43 -0300

    AIRCRAFT

--- AirCraftApp.java
+++ AirCraftApp.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class AirCraftApp
 {
     public static void main(String[] args)
     {
+        Scanner sc = new Scanner(System.in);
+        
+        AirCraft a =  new AirCraft();
         
     }
 }
